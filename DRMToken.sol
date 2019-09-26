2364e3e2524831dd7d57e68a5a7c9253c66b1e49
ohyung1287 <ohyung1287@gmail.com>
2019-09-26 15:27:53 -0400

    multi contributors

--- DRMToken.sol
+++ DRMToken.sol
@@ -7,10 +7,12 @@ contract DRMToken is ERC721BasicToken, Ownable{
      string public tokenName = "Digital Right Token";
     string public symbol = "DRM";
     struct Artwork{
+        string _artworkId;
         string name;
         string artist;
         string description;
-        string IPFS;// link of image 
+        string realart;// link of image 
+        string thumbnail;// url image
         uint64 timestamp;
     }
     uint _artworkId = 0;
@@ -20,13 +22,7 @@ contract DRMToken is ERC721BasicToken, Ownable{
     mapping(uint256 => uint256) public tokenPrices;
     // mapping(string => uint256[]) public IPFStoID; // filter out a list of ID with same image 
     uint public tokenCount;
-    function setTokenPrice(uint _tokenId, uint _price) external onlyOwner{
-        require(_exists(_tokenId));
-        tokenPrices[_tokenId] = _price;
-    }
-    // function findSimilar(string _IPFS) external returns(uint256[] artworks) {
-    //     return IPFStoID[_IPFS];
-    // }
+
     function artistRegister(address _artist, string _name)external onlyOwner{ 
         registeredArtists[_artist] = true;
     }
@@ -37,8 +33,11 @@ contract DRMToken is ERC721BasicToken, Ownable{
     function getTokenName()external returns(string){
         return tokenName;
     }
-    // Creates artwork as private token()
-    function ArtworkCreation(string _name, string _artist, string _description, string _IPFS, uint _deployNum) public { // When artist upload a new artwork to platform
+    // Creates artwork as private token
+    // 1. Put attributes in new token
+    // 2. Transfer token to the artist
+    // the new token will belong to artist directly
+    function privateCreation(string _name, string _artist, string _description, string _realart, string _thumbnail, uint _deployNum) public { // When artist upload a new artwork to platform
         require(_deployNum > 0);
         require(registeredArtists[msg.sender]);
         for(uint i=0; i < _deployNum ; i++){
@@ -46,13 +45,47 @@ contract DRMToken is ERC721BasicToken, Ownable{
                 name:_name,
                 artist:_artist,
                 description:_description,
-                IPFS:_IPFS,
+                realart:_realart,
+                thumbnail:_thumbnail,
                 timestamp:uint64(now)
             });
             uint256 id = artworks.push(_artwork) - 1;
-            tokenPrices[id] = 10000000000000000;// default price =0.01 eth
+            _mint(msg.sender, id);
             isCreated[id] = true;
-            // IPFStoID[_IPFS].push(id); // add image into mapping
+        }
+    }
+    function publicCreation(uint _price,string _name, string _artist, string _description, string _realart, string _thumbnail, uint _deployNum) public { // When artist upload a new artwork to platform
+        require(_deployNum > 0);
+        require(registeredArtists[msg.sender]);
+        for(uint i=0; i < _deployNum ; i++){
+            Artwork memory _artwork = Artwork({
+                name:_name,
+                artist:_artist,
+                description:_description,
+                realart:_realart,
+                thumbnail:_thumbnail,
+                timestamp:uint64(now)
+            });
+            uint256 id = artworks.push(_artwork) - 1;
+            tokenPrices[id] = _price * 1000000000000000000;
+            isCreated[id] = true;
+        }
+    }
+    function publicCreationWithContributor(uint _price, address[] _contribuctors, uint[] _percentages,string _name, string _artist, string _description, string _realart, string _thumbnail, uint _deployNum) public { // When artist upload a new artwork to platform
+        require(_deployNum > 0);
+        require(registeredArtists[msg.sender]);
+        for(uint i=0; i < _deployNum ; i++){
+            Artwork memory _artwork = Artwork({
+                name:_name,
+                artist:_artist,
+                description:_description,
+                realart:_realart,
+                thumbnail:_thumbnail,
+                timestamp:uint64(now)
+            });
+            uint256 id = artworks.push(_artwork) - 1;
+            tokenPrices[id] = _price * 1000000000000000000;
+            isCreated[id] = true;
         }
     }
     function requestBuy(uint256 _tokenId) payable external ifNotPaused {
@@ -62,12 +95,7 @@ contract DRMToken is ERC721BasicToken, Ownable{
         _mint(msg.sender, _tokenId); //Mints the token ID of the newest placeholder structure to that wallet address.
         tokenCount++; //adds one to total token count
     }
-    function purchaseInPeriod(uint _dateStart, uint _dateEnd, uint _tokenId)payable external returns(uint){// only sell in timeperiod
-        if(now >= _dateStart && now <= _dateEnd){
-            require(msg.value >= tokenPrices[_tokenId]);
-            TokenMint(msg.sender, _tokenId);
-        }
-    }
+
     modifier TokenMint(address _wallet, uint256 _tokenId){
         _mint(_wallet, _tokenId); //Mints the token ID of the newest placeholder structure to that wallet address.
         tokenCount++; //adds one to total token count
